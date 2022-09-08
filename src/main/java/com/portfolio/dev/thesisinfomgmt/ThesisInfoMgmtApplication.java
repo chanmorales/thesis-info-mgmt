79bbaa91a22d77a1f353783421d9f8f6f3bf822f
Christian Morales <christiancarven.morales@gmail.com>
2022-09-08 20:35:10 +0800

    TIM-1: Added sample controller and application file

---
+++ src/main/java/com/portfolio/dev/thesisinfomgmt/ThesisInfoMgmtApplication.java
@@ -0,0 +1,12 @@
+package com.portfolio.dev.thesisinfomgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisInfoMgmtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisInfoMgmtApplication.class, args);
+	}
+}
