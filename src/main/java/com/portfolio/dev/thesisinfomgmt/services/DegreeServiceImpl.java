c85eb84db16a01c6de5ef1ce5ff9159c9a251031
Christian Morales <christiancarven.morales@gmail.com>
2022-09-17 15:30:02 +0800

    TIM-20: Added validation method for degree details

--- src/main/java/com/portfolio/dev/thesisinfomgmt/services/DegreeServiceImpl.java
+++ src/main/java/com/portfolio/dev/thesisinfomgmt/services/DegreeServiceImpl.java
@@ -1,12 +1,18 @@
 package com.portfolio.dev.thesisinfomgmt.services;
 
+import static com.portfolio.dev.thesisinfomgmt.utilities.Constants.DEGREE_ABBR_ALREADY_EXISTS;
+import static com.portfolio.dev.thesisinfomgmt.utilities.Constants.DEGREE_ABBR_REQUIRED;
+import static com.portfolio.dev.thesisinfomgmt.utilities.Constants.DEGREE_NAME_REQUIRED;
+
 import com.portfolio.dev.thesisinfomgmt.dtos.DegreeDTO;
 import com.portfolio.dev.thesisinfomgmt.entities.Degree;
 import com.portfolio.dev.thesisinfomgmt.repositories.DegreeRepository;
 import com.portfolio.dev.thesisinfomgmt.utilities.MapperHelper;
+import com.portfolio.dev.thesisinfomgmt.utilities.ValidationResponse;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -84,5 +90,31 @@ public class DegreeServiceImpl implements DegreeService {
     } else {
       return Optional.empty();
     }
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  public ValidationResponse validateDegree(long degreeId, DegreeDTO degreeDTO) {
+
+    // Check if abbreviation is empty
+    if (StringUtils.isEmpty(degreeDTO.getAbbr())) {
+      return ValidationResponse.badRequest(DEGREE_ABBR_REQUIRED);
+    }
+
+    // Check if name is empty
+    if (StringUtils.isEmpty(degreeDTO.getName())) {
+      return ValidationResponse.badRequest(DEGREE_NAME_REQUIRED);
+    }
+
+    // Check if abbreviation already exists and with different id
+    Optional<Degree> degree = degreeRepository.findFirstByAbbrEqualsIgnoreCase(degreeDTO.getAbbr());
+    if (degree.isPresent() && degree.get().getId() != degreeId) {
+      return ValidationResponse.badRequest(
+          String.format(DEGREE_ABBR_ALREADY_EXISTS, degreeDTO.getAbbr()));
+    }
+
+    return ValidationResponse.ok();
   }
 }
--- src/main/java/com/portfolio/dev/thesisinfomgmt/utilities/Constants.java
+++ src/main/java/com/portfolio/dev/thesisinfomgmt/utilities/Constants.java
@@ -10,4 +10,8 @@ public class Constants {
   public static final String ROLE_NAME_REQUIRED = "Role name is required.";
   public static final String ROLE_NAME_ALREADY_EXISTS = "Role with name '%s' already exists.";
   public static final String ROLE_NOT_FOUND = "Role with id '%d' not found.";
+  public static final String DEGREE_ABBR_REQUIRED = "Degree abbreviation is required.";
+  public static final String DEGREE_NAME_REQUIRED = "Degree name is required.";
+  public static final String DEGREE_ABBR_ALREADY_EXISTS = "Degree with abbreviation '%s' already exists.";
+  public static final String DEGREE_NOT_FOUND = "Degree with id '%d' not found.";
 }
